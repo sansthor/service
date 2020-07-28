64663eb84d79814e07ebe4428b231103e91ae451
geraldos <sepdwijaya18@gmail.com>
2020-07-28 12:01:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const ejs = require('ejs');
+
+const { db } = require('./config');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('pages/home');
+});
+
+if (db) {
+    app.listen(PORT, () => {
+        console.log(`Server runs on port ${PORT}`);
+    });
+}
