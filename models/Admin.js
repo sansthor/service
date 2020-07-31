2cdae81027d3d7684b5d0e37d3ecf2c712be139d
geraldos <sepdwijaya18@gmail.com>
2020-07-31 15:18:16 +0700

    login admin

--- models/Admin.js
+++ models/Admin.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AdminSchema = new Schema({
-    id:{
+    email: {
         type:String,
-        required:true
+        required:true,
+        unique:true
+    },
+    username:{
+        type:String,
+        unique:true
     },
     password:{
         type:String,
---
+++ routes/Admin/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//AdminRegister
+router.post('/registerAdmin', require('./controller').registerAdmin);
+
+//AdminLogin
+router.post('/loginAdmin', require('./controller').loginAdmin);
+
+
+module.exports = router;
