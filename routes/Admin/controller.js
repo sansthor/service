cdc6b7ac2937891c583911afe6e0cb4aedeb3e07
geraldos <sepdwijaya18@gmail.com>
2020-07-31 15:05:40 +0700

    regis admin(just admin)

--- routes/Admin/controller.js
+++ routes/Admin/controller.js
@@ -1,4 +1,4 @@
-const {Admin, User} = require ('../../models')
+const {Admin} = require ('../../models')
 const {hash,compare} = require('../../helpers');
 const {createToken} = require('../../helpers');
 
@@ -15,44 +15,6 @@ module.exports = {
         }
         catch(error){
             res.send({message:'email sudah terdaftar'})
-        }
-    },
-    loginAdmin: async (req,res) =>{
-        try{
-            const {email, password} = req.body;
-            const registeredAdmin = await Admin.findOne({email:email})
-            
-            if(registeredAdmin !== null){
-                const compared = await compare(password,registeredAdmin.password)
-               if(compared === true){
-                const token = await createToken({
-                    id:registeredAdmin._id,
-                    email:registeredAdmin.email,
-                    username:registeredAdmin.username
-                })
-                
-                res.send({message:'log in success', result:token});
-               }
-               else{
-                res.status(403).send({message:'email or password incorrect'})
-            }
-        }
-          else{
-              res.status(403).send({message:'email not registered'})
-          } 
-        }
-    
-        catch(error){
-            console.log(error);
-            res.send({message:error.message})
-        }
-    },
-    getUser: async (req, res) => {
-        try {
-            const result = await User.find()
-            res.send({message:'get data', data:result})
-        } catch {
-            res.send({message:error.message})
         }
     }
 }
--- routes/Admin/index.js
+++ routes/Admin/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-//adminLogin
-router.post('/loginAdmin', require('./controller').loginAdmin);
+//AdminRegister
 router.post('/registerAdmin', require('./controller').registerAdmin);
-router.get('/getUser', require('./controller').getUser);
+
 
 module.exports = router;
