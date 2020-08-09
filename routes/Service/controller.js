4068c37374bd6f20a82232364b8a39e6e5b9fef4
renvelvet <reshapuspita734@gmail.com>
2020-08-09 15:26:36 +0700

    add descending to getallservices

--- routes/Service/controller.js
+++ routes/Service/controller.js
@@ -1,91 +1,82 @@
-const {
-    Service
-} = require('../../models')
+const { Service } = require('../../models');
 
 module.exports = {
     //dropdown category
     getFilterCategory: async (req, res) => {
-        const category = req.params
+        const category = req.params;
         try {
             const result = await Service.findOne({
-                category: category
-            }).exec()
+                category: category,
+            }).exec();
             res.send({
                 message: 'get filter services',
-                data: result
-            })
+                data: result,
+            });
         } catch (error) {
-            res.send(error)
+            res.send(error);
         }
     },
     postService: async (req, res) => {
         try {
             const result = await Service.create({
                 ...req.body,
-                userID: req.token.id
-            })
+                userID: req.token.id,
+            });
             res.send({
                 message: 'post success',
-                data: result
-            })
+                data: result,
+            });
         } catch (error) {
-            res.send(error)
+            res.send(error);
         }
     },
     getUserUpload: async (req, res) => {
-        const {
-            userID
-        } = req.params
+        const { userID } = req.params;
         try {
             const result = await Service.find({
-                userID
-            }).populate('userID')
+                userID,
+            }).populate('userID');
             res.send({
                 message: 'get user upload',
-                data: result
-            })
+                data: result,
+            });
         } catch (error) {
-            res.send(error)
+            res.send(error);
         }
     },
     //get detail service
     getDetails: async (req, res) => {
-        const {
-            id
-        } = req.params
+        const { id } = req.params;
         try {
-            const result = await Service.findById(
-                id
-            ).populate('userID')
+            const result = await Service.findById(id).populate('userID');
             res.send({
                 message: 'get details',
-                data: result
-            })
+                data: result,
+            });
         } catch (error) {
-            res.send(error)
+            res.send(error);
         }
     },
     deleteService: async (req, res) => {
-        const {
-            id
-        } = req.params
+        const { id } = req.params;
         try {
-            const result = await Service.findByIdAndDelete(id)
+            const result = await Service.findByIdAndDelete(id);
             res.send({
                 message: 'service deleted',
-                data: result
-            })
+                data: result,
+            });
         } catch (error) {
-            res.send(error)
+            res.send(error);
         }
     },
-    getAllServices: async (req,res) =>{
-        try{
-            const result = await Service.find().populate('userID')
-            res.send({message:'get all services', data:result})
+    getAllServices: async (req, res) => {
+        try {
+            const result = await Service.find()
+                .populate('userID')
+                .sort({ createdAt: 'desc' });
+            res.send({ message: 'get all services', data: result });
+        } catch (error) {
+            res.send(error);
         }
-        catch(error){
-            res.send(error)
-        }
-    }
-}
+    },
+};
