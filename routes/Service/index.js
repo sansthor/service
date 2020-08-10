969a1da5b8dea6beb7b0ea8b54611282fa55a88b
Miftah Faris <miftah.faris07@yahoo.co.id>
2020-08-10 11:27:44 +0700

    fix controller service

--- routes/Service/index.js
+++ routes/Service/index.js
@@ -5,9 +5,9 @@ const { verifyToken } = require('../../helpers');
 //get all services
 router.get('/', require('./controller').getAllServices);
 //get all best services
-router.get('/best', require('./controller').getAllServices);
+router.get('/best', require('./controller').getBestServices);
 //get all popular services
-router.get('/popular', require('./controller').getAllServices);
+router.get('/popular', require('./controller').getPopularServices);
 //get service details
 router.get('/details/:id', require('./controller').getDetails);
 // get service by user upload
